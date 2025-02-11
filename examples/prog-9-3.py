d256503233ff6d7c38ec0525346ebcf0aaaa1ddf
“y-hayakawa” <yoshinori.hayakawa@me.com>
2025-02-12 08:10:49 +0900

    データファイル配置調整

--- examples/prog-9-3.py
+++ examples/prog-9-3.py
@@ -171,8 +171,8 @@ def load_texed_model_from_obj_file(filename, shader, batch, has_normal=False):
 setup()
 
 # マテリアルデータの読み込み
-# "jupiter.obj", "jupiter.mtl", "2k_jupiter.jpg"をカレントディレクトリに置いておくこと
-model_jupiter = load_texed_model_from_obj_file(filename="jupiter.obj", shader=shader, batch=batch, has_normal=True)
+# テクスチャー画像（2k_jupiter.jpg）はカレントディレクトリに置いておくこと
+model_jupiter = load_texed_model_from_obj_file(filename="../data/jupiter.obj", shader=shader, batch=batch, has_normal=True)
 
 # 視点を設定
 window.view = Mat4.look_at(position=Vec3(0,0,3), target=Vec3(0,0,0), up=Vec3(0,1,0))
